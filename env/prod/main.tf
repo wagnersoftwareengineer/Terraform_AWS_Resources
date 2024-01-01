979ca9e5fb62fe093324f3722cc8a11d716cd479
Wagner Santos <wagnersfelipe@outlook.com>
2024-01-01 09:00:36 -0300

    Pipeline

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,11 +1,11 @@
 module "s3" {
-  source      = "module/s3"
+  source      = "../../module/s3"
   bucket_name = var.bucket_name
   acl_type    = var.acl_type
 }
 
 module "ec2" {
-  source        = "module/ec2"  # Certifique-se de que o caminho está correto.
+  source        = "../../module/ec2"  # Certifique-se de que o caminho está correto.
   region        = var.region           # Passa a região para o módulo.
   instance_type = var.instance_type    # Passa o tipo de instância para o módulo.
   key_name      = var.key_name         # Passa o nome da chave para o módulo.
